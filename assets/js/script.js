904a6896602ff27830cc1e139bc8a5b43b76f2fc
ManpreetRao <manpreetrao1@hotmail.com>
2023-03-05 12:12:28 +0000

    Add funtion to track what button has been selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log("complete")
+const numberOne = document.querySelector('.number1')
+const numberTwo = document.querySelector('.number2')
+
+function buttonClicked(guess) {
+    if (guess === 'higher') {
+        console.log('higher')
+    } else if (guess === 'lower') {
+        console.log('lower')
+    }
+}
