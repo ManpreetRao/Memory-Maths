080e5f4d7806c573e8bf24c84b661951a9a2b560
ManpreetRao <manpreetrao1@hotmail.com>
2023-03-05 12:45:43 +0000

    Add notes to javascript functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ let number1 = 0;
 let number2 = 0;
 let isHigherThanOne = false;
 
+//A function to validate whether the answer was correct
 function buttonClicked(guess) {
     if (guess === 'higher') {
         if (isHigherThanOne) {
@@ -24,15 +25,18 @@ function buttonClicked(guess) {
     gameLoop()
 }
 
+//A function to randomise the numbers
 function randomiseInt(max) {
     return Math.floor(Math.random() * (max + 1));
 
 }
 
+//A function to check if number 2 is higher than number 1
 function isHigher (number1, number2) {
     return number2 > number1;
 }
 
+//A function to insert the randomised numbers into the index file
 function gameLoop() {
     number1 = randomiseInt(10000);
     number2 = randomiseInt(10000);
