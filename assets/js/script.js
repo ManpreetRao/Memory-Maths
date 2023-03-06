b88744f474521008b757db55317cce8075c39cbd
ManpreetRao <manpreetrao1@hotmail.com>
2023-03-06 23:53:44 +0000

    Add high score tracker and change name of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ let number2 = 0;
 let isHigherThanOne = false;
 let currentScore = 0;
 let lives = 3000;
+let highScore = 0;
 
 //A function to validate whether the answer was correct
 async function buttonClicked(guess) {
@@ -73,7 +74,7 @@ function gameLoop() {
     if (lives < 100) {
         document.querySelector('.container').style.visibility = "hidden";
         document.querySelector('.result').style.visibility = "visible";
-        document.querySelector('#result-text').innerHTML = `<p>Awww.. You ran out of lives :(<br>Great attempt! You scored ${currentScore} points</p>`
+        document.querySelector('#result-text').innerHTML = `<p>Awww.. You ran out of lives :(<br>Great attempt! You scored ${currentScore} points</p><br><p>Your high score is ${highScore}</p>`
     }
     number1 = randomiseIntEasy(10000);
     number2 = randomiseIntEasy(10000);
@@ -82,6 +83,11 @@ function gameLoop() {
     numberTwo.innerHTML = number2;
     isHigherThanOne = isHigher(number1, number2);
     document.querySelector('#exact-lives').innerHTML = (lives / 1000);
+    if (highScore === 0) {
+        highScore = currentScore;
+    } else if (highScore < currentScore) {
+        highScore = currentScore;
+    }
 }
 
 gameLoop()
