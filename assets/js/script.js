d22ccf16d476d53e6b9b469dea034062c3929bc2
ManpreetRao <manpreetrao1@hotmail.com>
2023-03-06 23:47:37 +0000

    Change the timer to a health/lives system and add appropriate icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,29 @@ let number1 = 0;
 let number2 = 0;
 let isHigherThanOne = false;
 let currentScore = 0;
-let time = 5000;
+let lives = 3000;
 
 //A function to validate whether the answer was correct
 async function buttonClicked(guess) {
     if (guess === 'higher') {
         if (isHigherThanOne) {
-            result = 'Correct!';
+            result.innerHTML = 'Correct!';
             currentScore = ++currentScore;
-            time = time + 250;
+            lives = lives + 250;
         } else {
             result = 'Wrong :(';
-            time = time - 500;
+            lives = lives - 500;
         }
     }
 
      if (guess === 'lower') {
         if (isHigherThanOne) {
             result = 'Wrong :(';
-            time = time - 500;
+            lives = lives - 500;
         } else {
             result = 'Correct!';
             currentScore = ++currentScore;
-            time = time + 250;
+            lives = lives + 250;
         }
     }
 
@@ -44,7 +44,7 @@ async function buttonClicked(guess) {
 async function giveResult(result) {
     await setTimeout(() => {
         answer.innerHTML = '';
-    }, 1000)
+    }, 750)
 }
 
 //A function to randomise the numbers for first 5 levels
@@ -61,7 +61,7 @@ function isHigher (number1, number2) {
 //A function to allow you to reset the game score
 function resetScore() {
     currentScore = 0;
-    time = 5000;
+    lives = 3000;
     score.innerHTML = currentScore;
     document.querySelector('.container').style.visibility = "visible";
     document.querySelector('.result').style.visibility = "hidden";
@@ -70,21 +70,18 @@ function resetScore() {
 
 //A function to insert the randomised numbers into the index file
 function gameLoop() {
-    if (time < 100) {
+    if (lives < 100) {
         document.querySelector('.container').style.visibility = "hidden";
-        document.querySelector('.number1').style.visibility = "hidden";
-        document.querySelector('.number2').style.visibility = "hidden";
         document.querySelector('.result').style.visibility = "visible";
+        document.querySelector('#result-text').innerHTML = `<p>Awww.. You ran out of lives :(<br>Great attempt! You scored ${currentScore} points</p>`
     }
-    document.querySelector('.number1').style.visibility = "visible";
-    document.querySelector('.number2').style.visibility = "visible";
     number1 = randomiseIntEasy(10000);
     number2 = randomiseIntEasy(10000);
     
     numberOne.innerHTML = number1;
     numberTwo.innerHTML = number2;
     isHigherThanOne = isHigher(number1, number2);
-    document.querySelector('#exact-time').innerHTML = (time / 1000);
+    document.querySelector('#exact-lives').innerHTML = (lives / 1000);
 }
 
 gameLoop()
