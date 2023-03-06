aa0f22ddd3786945479a9b76c964b44ed1b4bcdf
ManpreetRao <manpreetrao1@hotmail.com>
2023-03-06 22:47:19 +0000

    Add element of time restrictions to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ let number1 = 0;
 let number2 = 0;
 let isHigherThanOne = false;
 let currentScore = 0;
+let time = 5000;
 
 //A function to validate whether the answer was correct
 async function buttonClicked(guess) {
@@ -14,8 +15,10 @@ async function buttonClicked(guess) {
         if (isHigherThanOne) {
             result = 'Correct!'
             currentScore = ++currentScore
+            time = time + 250
         } else {
             result = 'Wrong :('
+            time = time - 500
         }
     }
 
@@ -32,13 +35,14 @@ async function buttonClicked(guess) {
     answer.innerHTML = result;
 
     await giveResult()
+
+    gameLoop()
 }
 
 async function giveResult(result) {
     await setTimeout(() => {
         answer.innerHTML = '';
     }, 1000)
-    gameLoop()
 }
 
 //A function to randomise the numbers for first 5 levels
@@ -61,20 +65,14 @@ function resetScore() {
 
 //A function to insert the randomised numbers into the index file
 function gameLoop() {
-    
+    document.querySelector('.number1').style.visibility = "visible";
+    document.querySelector('.number2').style.visibility = "visible";
     number1 = randomiseIntEasy(10000);
     number2 = randomiseIntEasy(10000);
     
     numberOne.innerHTML = number1;
     numberTwo.innerHTML = number2;
     isHigherThanOne = isHigher(number1, number2);
-    hideNum(number1,number2)
 }
-
-function hideNum(number1, number2) {
-    document.getElementsByClassName('number1').style.visibility="hidden";
-    document.getElementsByClassName('number2').style.visibility="hidden";
-} 
-setTimeout("hideNum()", 2000);
 
 gameLoop()
